436c260c174a57c9bcf57ab497c24dc05323d9c0
Prasant Paudel <prashant.paudel.555@gmail.com>
2020-06-09 12:09:18 +0545

    simple cms built

--- courses/models.py
+++ courses/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
-# Create your models here.
+class Course(models.Model):
+    name = models.CharField(max_length=100)
+    url = models.CharField(max_length=200)
+    image = models.ImageField(upload_to='media/')
+    is_best_seller = models.BooleanField()
+
+    def __str__(self):
+        return self.name
+    
--- courses/views.py
+++ courses/views.py
@@ -1,4 +1,6 @@
 from django.shortcuts import render
+from .models import Course
 
 def home(request):
-    return render(request, 'courses/home.html')
+    courses = Course.objects
+    return render(request, 'courses/home.html', {'courses': courses})
