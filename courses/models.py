bfe3ab03c216c786c845e63c0e340baa4ed41736
Prasant Paudel <prashant.paudel.555@gmail.com>
2020-06-10 07:29:38 +0545

    model fields added -> expired, platform, duration, upload date

--- courses/models.py
+++ courses/models.py
@@ -1,11 +1,19 @@
 from django.db import models
 
+
 class Course(models.Model):
     name = models.CharField(max_length=100)
     url = models.CharField(max_length=200)
     image = models.ImageField(upload_to='media/')
-    is_best_seller = models.BooleanField()
+    best_seller = models.BooleanField(default=False)
+    expired = models.BooleanField(default=False)
+    platform = models.CharField(max_length=30, null=True)
+    duration = models.DurationField(null=True)
+    upload_date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
         return self.name
+    
+    # class Meta:
+    #     ordering = ['upload_date']
     
--- courses/views.py
+++ courses/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from .models import Course
+from django.db.models import F
 
 def home(request):
-    courses = Course.objects
+    courses = Course.objects.order_by('upload_date').reverse()
     return render(request, 'courses/home.html', {'courses': courses})
