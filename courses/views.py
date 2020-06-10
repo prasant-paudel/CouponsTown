8648be6b311946c8c2de4d47b5d6ae1ef809b36a
Prasant Paudel <prashant.paudel.555@gmail.com>
2020-06-10 20:35:14 +0545

    search functionality added

--- courses/views.py
+++ courses/views.py
@@ -7,13 +7,10 @@ def home(request):
     courses = Course.objects.order_by('upload_date').reverse()
     return render(request, 'courses/home.html', {'courses': courses})
 
+
 def search(request):
     template = 'courses/search.html'
     query = request.GET.get('search')
     query = query.strip("'").strip('"')
     results = Course.objects.filter(Q(name__contains=query))
-    print('\n\n')
-    print(query)
-    print('\n\n')
-
     return render(request, 'courses/search.html', {'courses': results, 'query': query})
