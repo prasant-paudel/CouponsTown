135201eb152245e94919f224670ddbd0b8f2300a
cris <cris@localhost.localdomain>
2020-07-13 11:12:24 +0545

    Coupon countdown added  and responsive update

--- courses/static/js/scripts.js
+++ courses/static/js/scripts.js
@@ -9,3 +9,12 @@
         document.getElementById("scroll-navbar").style.top = "-200px";
       }
     }
+
+
+window.onload = function(){
+  document.getElementById("enroll-button")[0].disabled = true;
+  setTimeout(function(){
+    var element = discount.getElementById("enroll-button")[0];
+    element.disabled = false;
+  },5000);
+}    
