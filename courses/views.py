f250936697c65267e2a555afc513d32a916b0399
Prasant Paudel <prashant.paudel.555@gmail.com>
2020-07-27 18:56:28 +0545

    static files at the end of body tag

--- courses/views.py
+++ courses/views.py
@@ -24,7 +24,7 @@ def get_queryset(keywords_list):
 def home(request):
     courses = Course.objects.order_by('image').order_by('upload_date').reverse()
     courses = courses.order_by('expired').reverse()
-    carousel2 = ((i,e) for (i,e) in enumerate(courses))
+    carousel2 = ((i,e) for (i,e) in enumerate(courses[:20]))
     return render(request, 'courses/landing.html', {'courses': courses, 'carousel2':carousel2})
 
 def courses(request):
